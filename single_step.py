a80c9695ce6bb34540b771a1f6b45bc441c7e09e
Kate <kate.tolstaya@gmail.com>
2019-03-26 14:43:14 -0400

    single

--- single_step.py
+++ single_step.py
@@ -5,7 +5,7 @@ from gym import wrappers
 import gym_flock
 
 parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
-parser.add_argument('--env-name', default="FlockingMulti-v0",
+parser.add_argument('--env-name', default="Consensus-v0",
                     help='name of the environment to run')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor for reward (default: 0.99)')
@@ -34,16 +34,17 @@ env = gym.make(args.env_name)
 
 state = env.reset()
 n_agents = 15
-n_features = 18
-n_actions = 2
+# n_features = 18
+# n_actions = 2
+n_features = 6
+n_actions = 1
 sigma = 0.01
 
 pi_w = np.random.rand(n_features, n_actions) * 2 - 1
 
-avg_avg_reward = 0
-
 for i_episode in range(args.num_episodes):
 
+    # train
     state = env.reset()
     step = 0
 
@@ -53,27 +54,38 @@ for i_episode in range(args.num_episodes):
 
         state = state.reshape(n_agents, n_features)
         pi_s = state.dot(pi_w)
-        action = pi_s + np.random.normal(0, sigma, size=(n_agents, 2))
+        action = pi_s + np.random.normal(0, sigma, size=(n_agents, n_actions))
         next_state, reward, done, _ = env.step(action.flatten())
-        #next_state, costs = next_state
 
         grad = np.zeros((n_features, n_actions))
 
         for i in range(n_agents):
             grad = grad + (action[i, :]-pi_s[i, :]).reshape(1, n_actions) * (state[i, :]).reshape(n_features, 1) #* costs[i]
 
-        pi_w = pi_w - 0.001 * grad * reward
+        pi_w = pi_w + 0.001 * grad * reward
 
         state = next_state
-
-        avg_reward = avg_reward + reward
 
         if done:
             break
 
-    avg_avg_reward = avg_avg_reward + avg_reward/200
-    if i_episode % 100 == 0:
-        print(avg_avg_reward/100)
-        avg_avg_reward = 0
+    # test
+    if i_episode % 100 == 0: 
+        state = env.reset()
+        avg_reward = 0
+        while True:
+            state = state.reshape(n_agents, n_features)
+            pi_s = state.dot(pi_w)
+            action = pi_s
+            next_state, reward, done, _ = env.step(action.flatten())
+
+            state = next_state
+
+            avg_reward = avg_reward + reward
+
+            if done:
+                break
+        print(avg_reward)
+
 
 
